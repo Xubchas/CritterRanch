08c8b962dc7ce4431838c1fec412c48805948963
Xubchas <114943483+Xubchas@users.noreply.github.com>
2022-10-12 19:36:52 -0500

    Shop Manager Base
    -added shop manager script and basic functionality

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
+    public GameObject upButton;
+    public GameObject downButton;
+
+    public List<GameObject> pages;
+    public int currentPage = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,37 @@ public class ShopManager : MonoBehaviour
     {
         
     }
+
+    public void ScrollUp(){
+        if(currentPage > 0){
+            pages[currentPage].SetActive(false);
+            currentPage++;
+            pages[currentPage].SetActive(true);
+        }
+
+        if(currentPage == 0){
+            upButton.SetActive(false);
+        }
+        if(currentPage < pages.Count-1){
+            downButton.SetActive(true);
+        }
+    }
+
+    public void ScrollDown(){
+        if(currentPage < pages.Count - 1){
+            pages[currentPage].SetActive(false);
+            currentPage--;
+            pages[currentPage].SetActive(true);
+        }
+
+        if(currentPage == pages.Count - 1){
+            downButton.SetActive(false);
+        }
+
+        if(currentPage > 0){
+            upButton.SetActive(true);
+        }
+    }
+
+
 }
